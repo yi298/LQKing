69b9a3fe9ca012219b2963368fa171d2b4eee31a
woo <1429715206@qq.com>
2019-11-21 14:42:29 +0800

    修改

--- js/commont.js
+++ js/commont.js
@@ -1,3 +1,7 @@
+//禁止页面选择以及鼠标右键
+document.οncοntextmenu = function () { return false; };
+document.onselectstart = function () { return false; };
+
 $(document).ready(function (e) {
   //轮播图
   $('#b01').unslider({
@@ -6,10 +10,26 @@ $(document).ready(function (e) {
   });
   //移动端nav
   $('.m-nav-btn').click(function () {
-    $('.m-nav').toggle();
+    $('.m-nav').slideToggle();
   });
   $('.m-nav__li--arrow').click(function () {
-    $(this).find('.m-nav__panel').toggle();
+    $(this).find('.m-nav__panel').slidetoggle();
   });
 
+  // 价格套餐切换
+  $("#on").click(function () {
+    $("#company").hide();
+    $("#person").show();
+  });
+  $("#on2").click(function () {
+    $("#company").show();
+    $("#person").hide();
+  });
+});
+
+
+var e = document.getElementById('btn');
+e.addEventListener('click', function () {
+  if (this.className == 'on') this.classList.remove('on');
+  else this.classList.add('on');
 });
